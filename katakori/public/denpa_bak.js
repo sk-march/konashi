2f8637189d093f29d4395502d69099c5f96f262a
finatech <tkoike@finatech.jp>
2014-06-24 21:34:40 +0900

    肩こり姿勢カウント機能

--- katakori/public/denpa_bak.js
+++ katakori/public/denpa_bak.js
@@ -7,30 +7,6 @@
 // view functions
 /////////////////////////////////////
 var mode;
-var cnt;
-var min = 1;
-//
-var maxMin = 30 * min;  // 60 min 定姿勢なら 、動作開始
-
-
-//var SEN11763P_ADDR = 0x68;
-var ITG3200_WHO = 0x00;
-var	ITG3200_SMPL = 0x15;
-var ITG3200_DLPF = 0x16;
-var ITG3200_INT_C = 0x17;
-var ITG3200_INT_S = 0x1A;
-var	ITG3200_TMP_H = 0x1B;
-var	ITG3200_TMP_L = 0x1C;
-var ITG3200_GX_H = 0x1D;
-var ITG3200_GX_L = 0x1E;
-var	ITG3200_GY_H = 0x1F;
-var	ITG3200_GY_L = 0x20;
-var ITG3200_GZ_H = 0x21;
-var ITG3200_GZ_L = 0x22;
-var ITG3200_PWR_M = 0x3E;
-
-
-jQuery = Zepto;
 $(function () {
     //console.log("aaa", location.href);
     if (location.href.match('3000')) {
@@ -46,65 +22,13 @@ $(function () {
         $("#button").on('click', function () {
             showConnecting();
             showMeter();
-
-            timerCount();
         });
     }
 
-    // タイマーリセット
-    $('#reset').on('tap click', function () {
-        cnt = 0;
-        $('#minCnt').text(cnt);
-    });
-
-    // video画面遷移
-    $('#videoButton').on('tap click', function () {
-        showVideo();
-    });
-
-    // ビデオ画面からの戻るボタン
-    $('#returnBtn').on('tap click', function () {
-        returnVideo();
-        cnt = 0;
-    });
 
 });
 
-
-
 /* 画面制御 */
-
-
-function timerCount() {
-    cnt = $('#minCnt').text();
-    //var cnt ++;
-
-    setInterval(function () {
-        cnt ++;
-        $('#minCnt').text(cnt);
-
-        if (cnt >= maxMin) {
-            //alert("aaaa");
-            showVideo();
-        }
-
-    }, 1000 * min);
-}
-
-function showVideo() {
-    $('#content').animate(
-        {left: "-300%"},
-        {duration: 500, easing: "ease-in-out"}
-    );
-}
-
-function returnVideo() {
-    $('#content').animate(
-        {left: "-200%"},
-        {duration: 500, easing: "ease-in-out"}
-    );
-};
-
 function showFirst() {
     $("#content").animate(
         {left: "0%"},
@@ -112,6 +36,12 @@ function showFirst() {
     );
 }
 
+function showTimeCount() {
+    $("#timeCount").animate(
+        {left: "-200%"},
+        {duration: 500, easing: "ease-in-out"}
+    );
+}
 
 function showConnecting() {
     $("#content").animate(
@@ -146,34 +76,10 @@ var intervalId;
 
 k.ready(function () {
     showMeter();
-    //k.pinModeAll(254);
-
-    //showPio();
-
-    k.i2cMode(k.KONASHI_I2C_ENABLE_400K);
-
-    //Initialize
-
-    k.i2cRestartCondition();
-    alert("a1");
-    var address = 0x4c;
-    //var length = KONASHI_I2C_DATA_MAX_LENGTH;
-
-
-
-    alert("a2");
+    k.pinModeAll(254);
 
     intervalId = window.setInterval(function () {
-        //k.signalStrengthReadRequest();
-        //alert("zzz");
-        try {
-            k.i2cReadRequest(10, address);
-            //k.i2cStartCondition();
-        } catch (e) {
-            alert(e);
-        }
-
-
+        k.signalStrengthReadRequest();
     }, 1000);
 });
 
@@ -182,15 +88,7 @@ k.on(k.KONASHI_EVENT_CONNECTED, function () {
     showConnecting();
 });
 
-k.on(k.KONASHI_EVENT_I2C_READ_COMPLETE, function () {
-    alert("aaa");
-});
-k.completeReadI2c(function (data) {
-    alert("bbb" + data);
-});
-
 /* 電波強度取得完了イベント */
-/*
 k.updateSignalStrength(function (data) {
     RSSI = -1 * data;    // 追加
     changeMeter(RSSI);   // 追加
@@ -203,12 +101,8 @@ k.updateSignalStrength(function (data) {
         k.pwmMode(k.PIO1, k.KONASHI_PWM_DISABLE);
     }
 });
-*/
-
 
 k.disconnected(function (data) {
     window.clearInterval(intervalId);
     showFirst();
 });
-
-
